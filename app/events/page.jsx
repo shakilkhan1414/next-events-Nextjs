35953783da2d68d015a5c08a1620627628adfc32
Shakil Khan <khan.shakil.1414@gmail.com>
2023-08-15 03:34:16 +0600

    initialize project

---
+++ app/events/page.jsx
@@ -0,0 +1,9 @@
+
+const page = () => {
+  return (
+    <div>Events</div>
+  )
+}
+
+export default page
+
---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main>
+      <h4>Home</h4>
+    </main>
+  )
+}
